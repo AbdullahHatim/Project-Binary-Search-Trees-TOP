0e4aae1a5dbb245d688ab33d52964e63aa689a92
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-04-21 14:08:51 -0700

    Initial commit

---
+++ Tree.js
@@ -0,0 +1,44 @@
+import { Node } from './Node.js'
+
+export class Tree {
+  constructor (arr) {
+    this.root = Tree.buildTree(arr)
+  }
+
+  static buildTree (arr = []) {
+    // Sort And Remove Duplicates
+    arr.sort((a, b) => a - b)
+    function removeDuplicates (arr) {
+      const cleanArr = []
+      arr.forEach(n => { if (!cleanArr.includes(n)) cleanArr.push(n) })
+      return cleanArr
+    }
+    const cleanArr = removeDuplicates(arr)
+    // Then Return the Tree
+    return Tree.getBalancedBST(cleanArr, 0, cleanArr.length - 1)
+  }
+
+  static getBalancedBST (arr = [], start, end) {
+    if (start > end) return null
+    const mid = Math.floor((start + end) / 2)
+
+    const root = new Node(arr[mid])
+    root.left = this.getBalancedBST(arr, start, mid - 1)
+    root.right = this.getBalancedBST(arr, mid + 1, end)
+
+    return root
+  }
+
+  static prettyPrint (node, prefix = '', isLeft = true) {
+    if (node === null) {
+      return
+    }
+    if (node.right !== null) {
+      Tree.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false)
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`)
+    if (node.left !== null) {
+      Tree.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true)
+    }
+  }
+}
