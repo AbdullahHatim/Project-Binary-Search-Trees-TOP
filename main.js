735f4b485c7993f7fec71a995d233ccdf2699c92
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-04-21 22:47:28 -0700

    📃 Wrote Basic tests

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
 import { Tree } from './Tree.js'
+function getRandomNumber (n = 100) {
+  const arr = []
+  for (let i = 0; i < n; i++) {
+    arr[i] = Math.floor(Math.random() * n)
+  }
+  return arr
+}
 
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-const tree = new Tree(arr)
-
-// tree.insert(6)
-tree.insert(6.5)
-// tree.insert(6.8)
-console.log(tree.height(tree.root.right.data))
-console.log(tree.height(tree.root.left.data))
-
-let sortedArr = []
-tree.inOrder(n => { sortedArr.push(n.data) })
-console.log(sortedArr)
+const tree = new Tree(getRandomNumber())
 Tree.prettyPrint(tree.root)
-// console.log(tree.depth(6.9))
-// console.log(tree.isBalanced())
+console.log('1- isBalanced(): ', tree.isBalanced())
+
+let preOrder = []
+tree.preOrder(n => preOrder.push(n.data))
+let inOrder = []
+tree.inOrder(n => inOrder.push(n.data))
+let postOrder = []
+tree.postOrder(n => postOrder.push(n.data))
+console.log(
+  `2- All Elements (Depth-First Traversal):
+  Pre Order: [${preOrder.toString()}]
+  \n
+  In Order: [${inOrder.toString()}]
+  \n
+  Post Order: [${postOrder.toString()}]
+  `)
+
+for (let i = 0; i < 10; i++) {
+  tree.insert(i + 101)
+}
+
+console.log('3- isBalanced(): ', tree.isBalanced())
+console.log('4- Balancing')
 tree.rebalance()
-console.log('After')
-sortedArr = []
-tree.inOrder(n => { sortedArr.push(n.data) })
-console.log(sortedArr)
+console.log('5- isBalanced(): ', tree.isBalanced())
 
-Tree.prettyPrint(tree.root)
+preOrder = []
+tree.preOrder(n => preOrder.push(n.data))
+inOrder = []
+tree.inOrder(n => inOrder.push(n.data))
+postOrder = []
+tree.postOrder(n => postOrder.push(n.data))
+console.log(
+  `6- All Elements (Depth-First Traversal):
+  Pre Order: [${preOrder.toString()}]
+  \n
+  In Order: [${inOrder.toString()}]
+  \n
+  Post Order: [${postOrder.toString()}]
+  `)
